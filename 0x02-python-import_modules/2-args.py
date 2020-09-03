b27b1f3c15661f44cd7fd077609dcb824432ccdd
Villasdaniel <2057@holbertonschool.com>
2020-09-03 18:48:26 -0500

    dos

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-c = len(sys.argv)
-if c == 1:
-    print("0 arguments.")
-elif c == 2:
-    print("1 argument:")
-elif c > 2:
-    print("{} arguments:".format(c-1))
-for i in range(1, c):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    c = len(sys.argv)
+    if c == 1:
+        print("0 arguments.")
+    elif c == 2:
+        print("1 argument:")
+    elif c > 2:
+        print("{} arguments:".format(c-1))
+    for i in range(1, c):
+        print("{}: {}".format(i, sys.argv[i]))
