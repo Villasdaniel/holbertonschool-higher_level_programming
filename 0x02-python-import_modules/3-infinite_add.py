70813d9f7c142a081a308ab3670bd5aaab4248d3
Villasdaniel <2057@holbertonschool.com>
2020-09-03 18:50:40 -0500

    tres

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-s = 0
-c = len(sys.argv)
-for i in range(1, c):
-    s = s + int(sys.argv[i])
-print(s)
+if __name__ == "__main__":
+    import sys
+    s = 0
+    c = len(sys.argv)
+    for i in range(1, c):
+        s = s + int(sys.argv[i])
+    print(s)
