be510272ac835146c75550a81771b6ce960c9cf5
Villasdaniel <2057@holbertonschool.com>
2020-09-03 19:18:47 -0500

    cuatro

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for a in dir(hidden_4):
-        print(a)
+    for i in dir(hidden_4):
+        if i[0] != "_"
+            print(i)
