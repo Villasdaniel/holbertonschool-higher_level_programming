88a30c7d3513c7d2e69bb28490f7c97e8edcce04
Villasdaniel <2057@holbertonschool.com>
2020-09-07 00:08:23 -0500

    header

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,9 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
-#include <stdio.h>
+
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,11 +12,6 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
 
 typedef struct listint_s
 {
