8bc720fc7a0ec1649571122bb7de1ef9ad44ec1a
Villasdaniel <2057@holbertonschool.com>
2020-09-08 18:19:19 -0500

    ocho

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+        if len(sentence) = 0:
+                y = None
+        else:
+                y = sentence[0]
         x = len(sentence)
-        y = sentence[0]
         tuple_a = (x, y)
         return(tuple_a)
