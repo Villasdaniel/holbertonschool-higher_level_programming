aa96e869d23cd206d570f7cb617748434dcaab71
Villasdaniel <2057@holbertonschool.com>
2020-09-08 17:33:39 -0500

    siete

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 x = tuple_a[0] + tuple_b[0]
                 y = tuple_a[1] + tuple_b[1]
         else:
-                x = (0 if len(tuple_a) == 0 else tuple_a[0]) + (0 if len(tuple_b) == 0 else tuple_b[0])
-                y = (0 if len(tuple_a) < 2 else tuple_a[1]) + (0 if len(tuple_b) < 2 else tuple_b[1])
-        tuple_c=(x, y)
+                x = (0 if len(tuple_a) == 0 else tuple_a[0]) +\
+                    (0 if len(tuple_b) == 0 else tuple_b[0])
+                y = (0 if len(tuple_a) < 2 else tuple_a[1]) +\
+                    (0 if len(tuple_b) < 2 else tuple_b[1])
+        tuple_c = (x, y)
         return(tuple_c)
