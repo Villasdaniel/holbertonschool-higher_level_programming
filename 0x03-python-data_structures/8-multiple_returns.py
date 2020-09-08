ee0f8d04decdc579997ea8e5da7b5eb8319a9615
Villasdaniel <2057@holbertonschool.com>
2020-09-08 18:21:00 -0500

    ocho

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
         if len(sentence) = 0:
-                y = None
+                y = "None"
         else:
                 y = sentence[0]
         x = len(sentence)
