16d4b91bbc19872cda19db90325782cadcb3904b
Villasdaniel <2057@holbertonschool.com>
2020-09-22 15:49:12 -0500

    cero.3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """class square"""
-class Square:
+class Square: pass
 
         """(python3 -c 'print(__import__("my_module").MyClass.__doc__)')"""
-        def __init__(self, Square={}):
-                self = {}
+
