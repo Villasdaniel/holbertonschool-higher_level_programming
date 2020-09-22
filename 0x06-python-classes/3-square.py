61cf5fb9ab17877abb29f1e3dc66f310a2f4d377
Villasdaniel <2057@holbertonschool.com>
2020-09-22 18:09:17 -0500

    tres

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""first class"""
+
+
+class Square:
+        """(python3 -c 'print(__import__("my_module").MyClass.__doc__)')
+        """
+        def __init__(self, size=0):
+                """variable initializing"""
+                self.__size = size
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
+
+        def area(self):
+                """method return current square area"""
+                return (self.__size * self.__size)
