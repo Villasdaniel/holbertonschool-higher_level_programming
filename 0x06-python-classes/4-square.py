acad100dae02ee4564f8cc7bb6826fbde33f5339
Villasdaniel <2057@holbertonschool.com>
2020-09-22 20:29:14 -0500

    cuatro

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""first class"""
+
+
+class Square:
+        """(python3 -c 'print(__import__("my_module").MyClass.__doc__)')
+        """
+        def __init__(self, size=0):
+                """variable initializing"""
+                self.__size = size
+
+        @property
+        def size(self):
+                """return value of size"""
+                return self.__size
+
+        @size.setter
+        def size(self, size):
+                """variable initializing"""
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
+                self.__size = size
+
+        def area(self):
+                """method return current square area"""
+                return (self.__size * self.__size)
