37cf3e892dcc5dbe9ac68aa6e0b63d318e7bdb5e
Villasdaniel <2057@holbertonschool.com>
2020-09-26 15:45:49 -0500

    cinco.1

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,13 +6,16 @@ prints a text with 2 new lines after each of these characters: ., ? and :
 """
 
 
-def text_indentation(text):
+def text_indentation(None):
         """
         prints a text with 2 new lines after characters: ., ? and :
         """
-        if type(text) not in [str]:
+        if type(text) is not str:
                 raise TypeError("text must be a string")
         text = text.replace('. ', '.\n\n')
+        text = text.replace('.', '.\n\n')
         text = text.replace('? ', '?\n\n')
+        text = text.replace('?', '?\n\n')
         text = text.replace(': ', ':\n\n')
+        text = text.replace(':', ':\n\n')
         print(text, end='')
