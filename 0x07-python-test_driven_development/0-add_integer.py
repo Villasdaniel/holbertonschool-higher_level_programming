bdce3df8750183acc381b8a1274807f0357ff7e0
Villasdaniel <2057@holbertonschool.com>
2020-09-25 22:47:33 -0500

    cero.1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,18 +7,8 @@ Function that addintegers
 
 
 def add_integer(a, b=98):
-    """add two integers
-
-    Args:
-        a (int, float): [an integer]
-        b (int, optional): [an integer]. Defaults to 98.
-
-    Raises:
-        TypeError: [if not an integer or float]
-        TypeError: [if not an integer or float]
-
-    Returns:
-        [type]: [the add of two integers]
+    """
+    add two integers
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
