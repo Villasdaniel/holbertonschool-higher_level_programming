0ca3c96caf0f4bdfa8d1d2112fa03e5e55975036
Villasdaniel <2057@holbertonschool.com>
2020-09-28 21:37:34 -0500

    todos

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class Rectangle:
+
+        """
+        class Rectangle
+        """
+        pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class Rectangle:
+        """
+        class Rectangle
+        """
+        def __init__(self, width=0, height=0):
+                """initialize instances"""
+                self.__width = width
+                self.__height = height
+
+        @property
+        def width(self):
+                """return width"""
+                return self.__width
+
+        @width.setter
+        def width(self, value):
+                """width set"""
+                if type(value) is not int:
+                        raise TypeError("width must be an integer")
+                if value < 0:
+                        raise ValueError("width must be >= 0")
+                else:
+                        self.__width = value
+
+        @property
+        def height(self):
+                """return height"""
+                return self.__height
+
+        @height.setter
+        def height(self, value):
+                """height set"""
+                if type(value) is not int:
+                        raise TypeError("height must be an integer")
+                if value < 0:
+                        raise ValueError("height must be >= 0")
+                else:
+                        self.__height = value
+
+        def area(self):
+                """method return current rectangle area"""
+                return (self.__width * self.__height)
+
+        def perimeter(self):
+                """method return current rectangle perimeter"""
+                if (self.__width == 0) or (self.__height == 0):
+                        return 0
+                return((self.__width * 2) + (self.__height * 2))
