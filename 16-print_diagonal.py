484a95a719918643cd7c21ea096a6942b1692078
Villasdaniel <66293956+Villasdaniel@users.noreply.github.com>
2020-09-28 22:00:55 -0500

    Delete 16-print_diagonal.py

--- 16-print_diagonal.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def print_diagonal(n):
-        for i in range(n):
-                for j in range(n + 1):
-                        if j == i:
-                                if n == 0:
-                                        print()
-                                else:
-                                        print("{}\\".format((i)* " "))
