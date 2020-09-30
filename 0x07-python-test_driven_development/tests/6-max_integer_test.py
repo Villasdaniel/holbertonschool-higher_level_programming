2c35d1276090136e378f91233fbc3dc8a2730ebf
Villasdaniel <2057@holbertonschool.com>
2020-09-29 19:55:46 -0500

    seis

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -19,48 +19,7 @@ class TestMaxInteger(unittest.TestCase):
         """empty list"""
         self.assertEqual(max_integer([]), None)
 
-    def test_tuple(self):
-        """random"""
-        with self.assertRaises(TypeError):
-            _max = max_integer((2, 3))
-
     def test_None(self):
         """random"""
         with self.assertRaises(TypeError):
             _max = max_integer(None)
-
-    def test_big_number(self):
-        """big numbers"""
-        _list = [2135456413213546541321324163463123132746,
-                 13524654351321324135461321354]
-        self.assertEqual(max_integer(_list),
-                         2135456413213546541321324163463123132746)
-
-    def test_float(self):
-        """floats"""
-        _list = [2.3, 5.6, 7.8]
-        with self.assertRaises(TypeError):
-            _max = max_integer(_list)
-
-    def test_inf(self):
-        _list = [float('inf'), float('inf')]
-        with self.assertRaises(TypeError):
-            _max = max_integer(_list)
-
-    def test_docstringmod(self):
-        """comments"""
-        m = __import__('6-max_integer').__doc__
-        self.assertTrue(m is not None and len(m) > 0)
-
-    def test_docstringfunc(self):
-        """comments"""
-        f = __import__('6-max_integer').max_integer.__doc__
-        self.assertTrue(f is not None and len(f) > 5)
-
-    def test_morethan(self):
-        """ones"""
-        with self.assertRaises(TypeError):
-            _max = max_integer(1, 1)
-
-if __name__ == '__main__':
-    unittest.main()
