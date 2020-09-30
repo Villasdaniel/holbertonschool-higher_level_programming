ed2c73cb540d094f80c127ad67093c9defe41e7b
Villasdaniel <2057@holbertonschool.com>
2020-09-29 19:50:34 -0500

    cinco

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,7 +12,7 @@ def text_indentation(text):
         """
         if type(text) is not str:
                 raise TypeError("text must be a string")
-        
+
         text = text.replace('.', '.\n\n')
         text = text.replace('?', '?\n\n')
         text = text.replace(':', ':\n\n')
