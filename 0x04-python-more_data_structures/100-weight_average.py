ea6c93f974843ebe791a84a815d279b0883f3f91
Villasdaniel <2057@holbertonschool.com>
2020-10-05 10:27:11 -0500

    cien

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+        if my_list == None:
+                return 0
         x = 0
         count = 0
         for i in my_list:
                 count += i[1]
                 x += i[0] * i[1]
-        print("{}".format(x))
-        print("{}".format(count))
         return (x/count)
