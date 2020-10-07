829f11c53b88824da90dbc4842f0d8c8b51f5092
Villasdaniel <2057@holbertonschool.com>
2020-10-07 17:41:50 -0500

    uno

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,7 +4,5 @@
 
 def number_of_lines(filename=""):
         """returns the number of lines of a text file"""
-
-
         with open(filename, 'r') as f:
                 return(sum(1 for line in f))
