aaf8801224920f5c32206786875a3d18d8a5bb09
Villasdaniel <2057@holbertonschool.com>
2020-10-06 22:50:36 -0500

    uno

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """class MyList"""
 
+
 class MyList(list):
-	""" prints the list, but sorted (ascending sort)"""
-	def print_sorted(self):
-		"""[summary]
-		"""
-		print(sorted(self))
+    """ prints the list, but sorted (ascending sort)"""
+    def print_sorted(self):
+        """[summary]"""
+        print(sorted(self))
