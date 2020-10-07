3c63a28b94ada5fabc73c117db06bfeee1192a08
Villasdaniel <2057@holbertonschool.com>
2020-10-06 21:59:33 -0500

    once

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""[summary]"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+        """empty class
+        """
+        def __init__(self, size):
+                """[summary]"""
+                self.integer_validator('size', size)
+                self.__size = size
+                super().__init__(size, size)
+
+        def area(self):
+                """area function"""
+                return self.__size * self.__size
+
+        def __str__(self):
+                """[summary]"""
+                return "[Square] {}/{}".format(self.__size, self.__size)
