ea61d74f2104f233cb53f6faa7b93e76a8aac4a5
Villasdaniel <2057@holbertonschool.com>
2020-10-07 17:27:22 -0500

    uno

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
         """reads a text file (UTF8) and prints it to stdout:"""
-        with open('my_file_0.txt', 'r') as f:
+        with open('my_file_0.txt') as f:
                 read_data = f.read()
                 print("{}".format(read_data))
