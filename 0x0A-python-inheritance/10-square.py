ebb140379413694e02957b4bfce4ff0cd6549867
Villasdaniel <2057@holbertonschool.com>
2020-10-06 21:53:21 -0500

    diez

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""[summary]"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+        def __init__(self, size):
+                self.integer_validator('size', size)
+                self.__size = size
+                super().__init__(size, size)
+
+        def area(self):
+                """area function"""
+                return self.__size * self.__size
