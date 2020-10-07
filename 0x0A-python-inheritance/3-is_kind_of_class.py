0012ffe36f38aba267d9b3d0372aa7eaf44d6e0c
Villasdaniel <2057@holbertonschool.com>
2020-10-06 19:01:23 -0500

    tres

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """[summary]"""
 
+
 def is_kind_of_class(obj, a_class):
         """returns True if object instance of a class, otherwise False"""
         return(isinstance(obj, a_class))
