97aa8d7186c1b2e126064e238254ac59f2570954
Villasdaniel <2057@holbertonschool.com>
2020-10-07 23:23:20 -0500

    doce

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -3,21 +3,20 @@
 
 
 class Student:
-        """class student"""
-        def __init__(self, first_name, last_name, age):
-                """init student"""
-                self.first_name = first_name
-                self.last_name = last_name
-                self.age = age
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        """init student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-        def to_json(self, attrs=None):
-                """returns the dictionary description"""
-                mydic = {}
-                if type(attrs) is list:
-                        for obj in attrs:
-                                if type(obj) is str and obj in self.__dict__.keys():
-                                        mydic[obj] = self.__dict__[obj]
-                        return mydic
-                else:
-                        return self.__dict__
-
+    def to_json(self, attrs=None):
+        """returns the dictionary description"""
+        mydic = {}
+        if type(attrs) is list:
+            for obj in attrs:
+                if type(obj) is str and obj in self.__dict__.keys():
+                    mydic[obj] = self.__dict__[obj]
+            return mydic
+        else:
+            return self.__dict__
