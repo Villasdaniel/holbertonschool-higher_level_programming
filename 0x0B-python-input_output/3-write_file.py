0c043c338fdc5151cb918e5b3b51e711614381f8
Villasdaniel <2057@holbertonschool.com>
2020-10-07 19:12:24 -0500

    tres

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""writes a string to a text file (UTF8) and returns the number of characters"""
+"""writes a text file (UTF8) and returns the number of characters"""
 
 
 def write_file(filename="", text=""):
-        """writes a string to a text file (UTF8) and returns the number of characters"""
+        """writes a text file (UTF8) and returns the number of characters"""
         with open(filename, 'w', encoding="utf-8") as f:
                 return(f.write(text))
