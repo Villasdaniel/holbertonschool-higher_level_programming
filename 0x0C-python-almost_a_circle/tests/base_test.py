3258c8a8e79af1562ae928cdd10be36b2abb83a3
Villasdaniel <2057@holbertonschool.com>
2020-10-13 18:25:36 -0500

    tests

---
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""unittest base"""
+
+import unittest
+from models.base import Base
+import inspect
+import pep8
+
+
+class base_test(unittest, testcase):
+"""unittest base"""
+    def test_doctstring(self):
+        """Test documentation"""
+        print(mrdoc)
+        self.assertTrue(len(mrdoc.strip()) > 0)
+        self.assertTrue(len(Base.__doc__.strip()) > 0)
+        functions = inspect.getmembers(Base, predicate=inspect.ismethod)
+        for name, func in functions:
+            self.assertTrue(len(func.__doc__.strip()) > 0)
+        functions = inspect.getmembers(Base, predicate=inspect.isfunction)
+        for name, func in functions:
+            self.assertTrue(len(func.__doc__.strip()) > 0)
+
+    def test_pep8(self):
+        """Test PEP8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/base.py"])
+        self.assertEqual(result.total_errors, 0)
+
+if __name__ == "__main__":
+    unittest.main()
