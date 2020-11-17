b3bd806ea7fddbe0edefedf4542a6c56f7ab4ef3
Daniel Villa <2057@holbertonschool.com>
2020-11-17 16:45:19 -0500

    seven

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,2 +1,3 @@
+-- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY (id) REFERENCES states(id), name VARCHAR(256)NOT NULL);
