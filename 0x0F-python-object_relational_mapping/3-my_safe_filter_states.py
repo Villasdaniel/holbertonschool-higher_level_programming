3a654f659b446419caa33e4608277e1303510414
daniel <2057@holbertonschool.com>
2020-12-12 18:07:23 -0500

    three

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,17 +7,16 @@ from sys import argv
 
 
 def get_states(username, password, dbname, match):
-        """lists all states with a name starting with N from the database"""
-        db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
-                             passwd=str(password), db=str(dbname), charset="utf8")
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name=%(name)s\
-        ORDER BY id", {'name': match})
-        rows = cur.fetchall()
-        for row in rows:
-                print(row)
-        cur.close()
-        db.close()
+    """lists all states with a name starting with N from the database"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(dbname), charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%(name)s", {'name': match})
+    rows = cur.fetchall()
+    for row in rows:
+            print(row)
+    cur.close()
+    db.close()
 
 if __name__ == '__main__':
         get_states(argv[1], argv[2], argv[3], argv[4])
