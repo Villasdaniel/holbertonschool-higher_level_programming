b3c934ce0770f0d10e4863bc31029578d7d6bce5
daniel <2057@holbertonschool.com>
2020-12-12 18:09:04 -0500

    four

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,17 +7,17 @@ from sys import argv
 
 
 def get_cities_states(username, password, dbname):
-        """lists all cities from the database hbtn_0e_4_usa"""
-        db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
-                             passwd=str(password), db=str(dbname), charset="utf8")
-        cur = db.cursor()
-        cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-        JOIN states ON cities.state_id=states.id ORDER BY id")
-        rows = cur.fetchall()
-        for row in rows:
-                print(row)
-        cur.close()
-        db.close()
+    """lists all cities from the database hbtn_0e_4_usa"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(dbname), charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id=states.id")
+    rows = cur.fetchall()
+    for row in rows:
+            print(row)
+    cur.close()
+    db.close()
 
 if __name__ == '__main__':
-        get_cities_states(argv[1], argv[2], argv[3])
+    get_cities_states(argv[1], argv[2], argv[3])
