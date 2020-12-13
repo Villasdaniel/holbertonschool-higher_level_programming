ea0cc6432ad469d61216d94686671590bd3feb9c
daniel <2057@holbertonschool.com>
2020-12-13 10:40:06 -0500

    eight

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,9 +12,14 @@ if __name__ == "__main__":
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
+    session = Session()
     state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}". format(state.id, state.name))
-    else:
+    try:
+        id, name = session.query(State.id, State.name).from_statement(text(
+                "SELECT id, name "
+                "FROM states "
+        )).first()
+        print("{:d}: {:s}".format(id, name))
+    except TypeError:
         print('nothing')
     session.close()
