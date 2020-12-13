86a215a522a9e3f5744d27e5fabee7f6ba1513d8
daniel <2057@holbertonschool.com>
2020-12-13 11:30:13 -0500

    eleven

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            sys.argv[1], sys.argv[2], sys.argv[3]))
+                            argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     session = Session(engine)
     stad = State(name="Louisiana")
