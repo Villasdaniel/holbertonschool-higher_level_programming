59103a4b1434c6dec943e869518f53751e26644d
daniel <2057@holbertonschool.com>
2020-12-13 10:11:18 -0500

    eleven

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -3,12 +3,12 @@
 """
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'. format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                             sys.argv[1], sys.argv[2], sys.argv[3]))
     session = Session(engine)
     stad = State(name="Louisiana")
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,12 +3,12 @@
 """
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'. format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                             argv[1], argv[2], argv[3]))
     session = Session(engine)
     state = session.query(State).order_by(State.id).first()
