ceede86abe509a4beb4d2312f5f0bef326b5d9ff
daniel <2057@holbertonschool.com>
2020-12-21 10:57:10 -0500

    fix six and nine

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes request 0.0.0.0:5000/catch_me causes server to respond with message, in body of response
-curl -s -H --data @catch_me 0.0.0.0:5000/catch_me
+curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,11 +2,14 @@
 """inds a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
-    '''inds a peak in a list of unsorted integers'''
-    if list_of_integers:
-        if len(list_of_integers) % 3 == 0:
-            list_of_integers.pop(0)
-            return max(list_of_integers)
-        else:
-            return max(list_of_integers)
-    return None
+    """inds a peak in a list of unsorted integers"""
+    if len(list_of_integers) == 0:
+        return None
+    arr, leng = list_of_integers, len(list_of_integers)
+    half = leng // 2
+    if (half == leng - 1 or arr[half] >= arr[half + 1]):
+        if (half == 0 or arr[half] >= arr[half - 1]):
+            return arr[half]
+    if half != leng - 1 and arr[half + 1] > arr[half]:
+        return find_peak(arr[half + 1:])
+    return find_peak(arr[:half])
