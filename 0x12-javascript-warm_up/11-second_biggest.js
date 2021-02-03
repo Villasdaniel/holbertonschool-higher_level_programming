f5058610d62d118e0c79add59a1fce02555abafa
daniel <2057@holbertonschool.com>
2021-02-02 19:59:01 -0500

    fixed eleven

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments
 
-let MyVar = []; let i;
+const MyVar = []; let i;
 for (i = 2; i < process.argv.length; i++) {
-    MyVar.push(parseInt(process.argv[i]));
+  MyVar.push(parseInt(process.argv[i]));
 }
 if (MyVar.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
   MyVar.sort();
   console.log(MyVar[MyVar.length - 2]);
