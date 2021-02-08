8eb960f8b498114254b19991a76060d95a900fff
daniel <2057@holbertonschool.com>
2021-02-08 17:01:50 -0500

    fixed eight

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // returns the reversed version of a list
 
 exports.esrever = function (list) {
-  let e = [];
+  const e = [];
   for (let i = list.length - 1; i >= 0; i--) {
     e.push(list[i]);
   }
